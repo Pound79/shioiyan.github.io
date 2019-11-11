77a1118b7ec3a2e70621d6dd8e68db08c0e1095c
Kanta Matsui <kma9879@gmail.com>
2019-11-11 18:10:59 +0900

    add bgm test

---
+++ auto-start-touch-resume/index.js
@@ -0,0 +1,24 @@
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+const ctx = new AudioContext();
+
+const request = new XMLHttpRequest();
+const url = 'https://zprodev.github.io/web-audio-test/assets/Campfire_Song.mp3';
+request.open('GET', url, true);
+request.responseType = 'arraybuffer';
+request.onload =  () => {
+  ctx.decodeAudioData(request.response, (audioBuffer) => {
+    const audioSource = ctx.createBufferSource();
+    audioSource.buffer = audioBuffer;
+    audioSource.connect(ctx.destination);
+    audioSource.start();
+  });
+}
+request.send();
+
+const eventName = typeof document.ontouchend !== 'undefined' ? 'touchend' : 'mouseup';
+document.addEventListener(eventName, initAudioContext);
+function initAudioContext(){
+  document.removeEventListener(eventName, initAudioContext);
+  // wake up AudioContext
+  ctx.resume();
+}
